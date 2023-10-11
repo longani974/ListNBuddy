1c4a7e17055c33706257df80657a614f1a9a4952
Sylvain Riviere <sylv1.riviere@gmail.com>
2023-10-11 11:48:00 +0400

    ads bottom padding

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -356,12 +356,12 @@ const Table: React.FC<Lists> = (data) => {
         </tr>
     </tfoot> */}
                     </table>
-
+                    <div className="e pr-2 pl-2">
                         <Adsense
                             client="ca-pub-1943996794458760"
                             slot="6761017378"
                             format="auto"
-                        />
+                        /></div>
                     {(!!localStorageLists.length || data.id) && (
                         <div className="flex justify-end">
                             <div
